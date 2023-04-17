f3e44d8285fe6fb5ac4794831377cad5b5d11d1e
KatiaZd <katia.zouad@hotmail.fr>
2023-04-17 14:35:25 +0200

    feat : Création des pages + header et footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ArticlesPageComponent
   ],
   imports: [
     BrowserModule,
