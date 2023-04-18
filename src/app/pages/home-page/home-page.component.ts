d4516a902b1355ca32843afb77554a5d50f799a4
KatiaZd <katia.zouad@hotmail.fr>
2023-04-18 14:52:29 +0200

    feat : homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ARTICLES } from 'src/app/mocks/articles.mock';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+
+  // Récupération du mock de données articles.mock.ts
+  articles = ARTICLES;
+  
+
+
+
+
+  
+
 
 }
