7f08d09a643826ab51c0edccaac9c2b121dc1961
KatiaZd <katia.zouad@hotmail.fr>
2023-05-03 12:02:15 +0200

    feat : affichage du composant resumArticle dans la page Article

--- src/app/components/resum-article/resum-article.component.ts
+++ src/app/components/resum-article/resum-article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resum-article',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./resum-article.component.css']
 })
 export class ResumArticleComponent {
+
+  @Input() resumText!: string;
 
 }
