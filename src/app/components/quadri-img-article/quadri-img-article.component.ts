d3efbc6801e7d2be80aec042048725c11701fa71
KatiaZd <katia.zouad@hotmail.fr>
2023-05-05 14:23:14 +0200

    feat : Affichage de la gallerie d'img dans la page articles

--- src/app/components/quadri-img-article/quadri-img-article.component.ts
+++ src/app/components/quadri-img-article/quadri-img-article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quadri-img-article',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./quadri-img-article.component.css']
 })
 export class QuadriImgArticleComponent {
+
+  @Input() gallery: string[] = [];
 
 }
